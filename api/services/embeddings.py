3e7102e0f242dc827663887a710328c826e205b1
Otavio Landim <otavio.landim@gmail.com>
2026-09-09 10:33:47 -0300

    feat: add project struture

---
+++ api/services/embeddings.py
@@ -0,0 +1,16 @@
+from config.settings import settings
+from fastembed import LateInteractionTextEmbedding, SparseTextEmbedding, TextEmbedding
+
+
+class EmbeddingService:
+    def __init__(self):
+        self.dense_model = TextEmbedding(settings.dense_model)
+        self.sparse_model = SparseTextEmbedding(settings.sparse_model)
+        self.colbert_model = LateInteractionTextEmbedding(settings.colbert_model)
+
+    def embed_query(self, query: str):
+        dense = list(self.dense_model.query_embed([query]))[0].tolist()
+        sparse = list(self.sparse_model.query_embed([query]))[0].as_object()
+        colbert = list(self.colbert_model.query_embed([query]))[0].tolist()
+
+        return dense, sparse, colbert
