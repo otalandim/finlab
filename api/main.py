62995c5c2500f72646bcd1cff2f10f31ea220c1b
Otavio Landim <otavio.landim@gmail.com>
2026-09-09 11:05:56 -0300

    feat: Adding RAG API

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from config.settings import settings
 from fastapi import FastAPI
+from models.rag import RAGRequest, RAGResponse
 from models.search import SearchRequest, SearchResponse
+from services.rag import RAGService
 from services.search import SearchService
 
 app = FastAPI(title="Financial Search API")
@@ -11,10 +13,17 @@ search_service = SearchService(
     collection_name=settings.collection_name,
 )
 
+rag_service = RAGService(search_service=search_service)
+
 
 @app.post("/search", response_model=SearchResponse)
 def search(request: SearchRequest):
     return search_service.search(request.query, request.limit)
+
+
+@app.post("/rag", response_model=RAGResponse)
+def rag(request: RAGRequest):
+    return rag_service.generate_answer(request.query, request.limit)
 
 
 @app.get("/", name="on", description="online")
